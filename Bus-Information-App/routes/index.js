54c06c1e7ca8b83061aa7eed871c61d357e430d5
sangumee <super2451894@gmail.com>
2018-08-02 12:00:54 +0900

    package.json update
    supervisor

--- Bus-Information-App/routes/index.js
+++ Bus-Information-App/routes/index.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
+const pug = require('pug');
+const parser = require('xml2json');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+var url = 'http://openapi.its.ulsan.kr/UlsanAPI/RouteInfo.xo';
+var queryParams = '?' + encodeURIComponent('ServiceKey') + '=서비스키'; /* Service Key*/
+queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('1'); /* 기본1 */
+queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('10'); /* 기본10 */
+
+
+request({
+  url: url + queryParams,
+  method: 'GET'
+}, function (error, response, body) {
+  //console.log('Status', response.statusCode);
+  //console.log('Headers', JSON.stringify(response.headers));
+  //console.log('Reponse received', body);
+
+  /* GET home page. */
+  router.get('/', function (req, res, next) {
+    res.render('index', {
+      title: 'Express'
+    });
+  });
 });
+
+
 
 module.exports = router;
